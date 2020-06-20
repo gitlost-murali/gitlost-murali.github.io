a70eb204ae0f5f895839454246988aa0173fa1b4
gitlost-murali <kmanoharmurali@gmail.com>
2020-06-21 01:21:31 +0530

    Updating folder structures. Along with files

--- updatecount.php
+++
@@ -1,8 +0,0 @@
-<?php
-$myFile = "views.json";
-$strg = file_get_contents($myFile);
-$json = json_decode($strg, true);
-$json['views'] = $json['views'] + 1;
-$newJsonString = json_encode($json);
-file_put_contents($myFile, $newJsonString);
-?>
