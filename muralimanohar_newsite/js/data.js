f0ef4d926c4f34785f6e7d4f0e7dbe70a087d2ec
gitlost-murali <kmanoharmurali@gmail.com>
2020-06-21 00:59:46 +0530

    terminal site updated with thesis info. New site added

---
+++ muralimanohar_newsite/js/data.js
@@ -0,0 +1,45 @@
+data = {
+    "work": [{
+        "name": "EmoBGM",
+        "description": "A Novel & fun approach to Video Analysis through BGM",
+        "image": "ipanalysis.png",
+        "linktext": "<i class='fas fa-server'></i> Website",
+        "link": "https://ai.gramener.com/bgm/"
+      },
+      {
+        "name": "Document Risk Analyzer",
+        "description": "Identifies risk level of each clause in an agreement and also highlights key terms.",
+        "image": "vijaykarnataka.jpg",
+        "linktext": "<i class='fas fa-server'></i> Website",
+        "link": "https://ai.gramener.com/smartcontract/"
+      },
+      {
+        "name": "Determining Relevance in Visual QA",
+        "description": "Check if question asked is relevant to the image. This saves the VQA inference time.",
+        "image": "wildlife.png",
+        "linktext": "<i class='fab fa-github'></i> GitHub",
+        "link": "https://github.com/gitlost-murali/Question-Relevance-in-Visual-QA"
+      },
+      {
+        "name": "CameraTraps",
+        "description": "Worked on EfficientDet for CameraTraps. Developed at Gramener in partnership with Microsoft",
+        "image": "pptx.png",
+        "linktext": "<i class='fab fa-github'></i> GitHub",
+        "link": "https://github.com/gitlost-murali/CameraTraps"
+      },
+      {
+        "name": "Thesis on Unsupervised NMT",
+        "description": "Worked on making a robust Unsupervised NMT for low-resource languages. Supervised by Prof.S Sanyal & Dr. Manish at IIIT-H",
+        "image": "comicgen.jpg",
+        "linktext": "<i class='fab fa-github'></i> GitHub",
+        "link": "https://github.com/gitlost-murali/UnsupervisedMT"
+      },
+      {
+        "name": "TextPlatform (In progress)",
+        "description": "An open-source effort to help people with their datasets on augmentation/finding relatable datasets.",
+        "image": "cartogram.jpeg",
+        "linktext": "<i class='fab fa-github'></i> GitHub",
+        "link": "https://github.com/gitlost-murali"
+      }
+    ]
+  }
